332f35bfc551912e84ad0add12497f8eefdce837
kristian <k3nder@outlook.es>
2025-04-06 14:00:38 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 echo "Installing FIMAKE..."
 mkdir ~/.config/fimake
 set -U FIMAKE_HOME "$HOME/.config/fimake/"
-echo "
-function fimake
-    source $HOME/.config/fimake/src/main.fish $argv[1]
-end
-" > ~/.config/fish/functions/fimake.fish
+echo "source $HOME/.config/fimake/src/main.fish $argv[1]" >> ~/.config/fish/config.fish
 git clone https://github.com/k3nder/fimake.git $FIMAKE_HOME
 rm $FIMAKE_HOME/README.md
 rm $FIMAKE_HOME/install.sh
