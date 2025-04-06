579d49c460cc57ab0330dc87cf0a56ad40676f3e
kristian <k3nder@outlook.es>
2025-04-06 14:00:04 +0000

    Update main.fish

--- src/main.fish
+++ src/main.fish
@@ -1,18 +1,20 @@
-for func in $FIMAKE_HOME/src/functions/*
-    source $func
-end
-if test "$argv[1]" = "new"
-    if not set -q argv[2]
-        read -P "Proyect name: " name
-        fimake_new "$name"
-    else
-        if not set -q argv[3]
-            fimake_error "Proyect name not found"
-        end
-        fimake_frtem "$argv[2]" "$argv[3]"
+function fimake
+    for func in $FIMAKE_HOME/src/functions/*
+        source $func
     end
-else if test "$argv[1]" = "load"
-    fimake_load
-else
-    fimake_error "function not found" 1
+    if test "$argv[1]" = "new"
+        if not set -q argv[2]
+            read -P "Proyect name: " name
+            fimake_new "$name"
+        else
+            if not set -q argv[3]
+                fimake_error "Proyect name not found"
+            end
+            fimake_frtem "$argv[2]" "$argv[3]"
+        end
+    else if test "$argv[1]" = "load"
+        fimake_load
+    else
+        fimake_error "function not found" 1
+    end
 end
